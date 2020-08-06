2f1f1ee4850ba00507caed14e8ba2a9f45ed347e
harikix <ssk960809@gmail.com>
2020-08-06 15:16:48 +0900

    ファイル追加

---
+++ src/components/CommonFooter.vue
@@ -0,0 +1,27 @@
+<template>
+  <footer id="footer">
+    <small class="small">Copyright 2019 estra inc. All Rights Reserved.</small>
+  </footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+#footer {
+  height: 50px;
+  background-color: #000000;
+  text-align: center;
+}
+.small {
+  line-height: 50px;
+  color: #fff;
+}
+</style>
